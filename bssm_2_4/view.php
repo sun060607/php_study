cd71a079e7070bbfc606fdef5601c4c76a8ed713
박민성 <minsung060607@gmail.com>
2023-04-11 12:16:14 +0900

    Add files via upload

--- bssm_2_4/view.php
+++ bssm_2_4/view.php
@@ -10,8 +10,16 @@
      while($row = mysqli_fetch_assoc($result)){
         echo"<option value='".$row['did']."'>".$row['did']."</option>";
      }
+     //갯수를 10개 20개 30개 중에 선택하고 기본값은 10으로한다!
+     
   ?>
 </select>
+<br>
+<input type = radio name = limt value=10 checked> 10개<br>
+<input type = radio name = limt value=20> 20개<br>
+<input type = radio name = limt value=30> 30개<br>
+<input type = radio name = order value=asc checked>오름차순<br>
+<input type = radio name = order value=desc>내림차순<br>
 <input type = submit value=확인>
     </form>
 <?php
@@ -23,9 +31,10 @@
   //MYSQL연결한다
    $conn = mysqli_connect('localhost', 'root', '','bssm2_4');
    //데이터를 읽어오는 쿼리를 작성한다
-   $query = "select * from sensor where did='".$_POST['did']."';";
+   $query = "select * from sensor where did='".$_POST['did']."' order by num ".$_POST['order']." limit ".$_POST['limt'].";";
    //쿼리를 실행한다
    $result = mysqli_query($conn, $query);
+   echo $query;
    //결과를 출력한다
  
    echo "<table border=1 width=500>"; 
